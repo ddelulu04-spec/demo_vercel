cc2818a50e1244363292be2a089b11a1f9c4db7f
Bika <ddelulu04@gmail.com>
2026-07-24 13:36:43 +0500

    Explicitly configure cloudinary in settings.py

--- intellihire_web/settings.py
+++ intellihire_web/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import cloudinary
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,6 +96,13 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
+
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
+    secure=True
+)
 
 STORAGES = {
     "default": {
